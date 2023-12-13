cdb475c68268c53b28007f18851135dfa7bdbf1e
Pantelis Zoupis <pantelis.zoupis@gmail.com>
2023-12-13 22:43:44 +0200

    Solve day 11

--- src/main/java/me/zoupis/adventofcode/year2023/Day11.java
+++ src/main/java/me/zoupis/adventofcode/year2023/Day11.java
@@ -13,16 +13,24 @@ public class Day11 {
   private static final Logger LOGGER = LogManager.getLogger(Day11.class);
   private static final InputHandler INPUT_HANDLER = InputHandler.getInstance();
 
-  private static final int EXPANSION_RATE = 10;
+  private static final int EXPANSION_RATE = 1000000;
 
   public static void main(String[] args) {
     var input = INPUT_HANDLER.readInputFile("adventofcode/year2023/day11.input");
 
     LOGGER.info(part1(input));
+
+    input = INPUT_HANDLER.readInputFile("adventofcode/year2023/day11.input");
+    LOGGER.info(part2(input));
   }
 
   private static long part1(List<String> input) {
     input = expandUniverse(input);
+    var galaxies = getAllGalaxiesPart1(input);
+    return calculateShortestPathBetweenEveryPairOfGalaxies(galaxies);
+  }
+
+  private static long part2(List<String> input) {
     var galaxies = getAllGalaxies(input);
     return calculateShortestPathBetweenEveryPairOfGalaxies(galaxies);
   }
@@ -37,7 +45,7 @@ public class Day11 {
     return sum;
   }
 
-  public static List<Point> getAllGalaxies(List<String> input) {
+  public static List<Point> getAllGalaxiesPart1(List<String> input) {
     List<Point> galaxies = new ArrayList<>();
     for (int i = 0; i < input.size(); i++) {
       var indexes = getAllIndexesOf("#", input.get(i));
@@ -48,6 +56,40 @@ public class Day11 {
       galaxies.addAll(points);
     }
     return galaxies;
+  }
+
+  public static List<Point> getAllGalaxies(List<String> input) {
+    List<Integer> verticalIndexes = getAllVerticalIndexes(input);
+    List<Integer> horizontalIndexes = getAllHorizontalIndexes(input);
+
+    List<Point> galaxies = new ArrayList<>();
+    for (int i = 0; i < input.size(); i++) {
+      var indexes = getAllIndexesOf("#", input.get(i));
+      int finalI = i;
+      var points = indexes.stream()
+        .map(x -> getPoint(x, finalI, verticalIndexes, horizontalIndexes))
+        .toList();
+      galaxies.addAll(points);
+    }
+    return galaxies;
+  }
+
+  private static Point getPoint(final int x,
+                                final int y,
+                                List<Integer> verticalIndexes,
+                                List<Integer> horizontalIndexes) {
+    int countX = (int) horizontalIndexes.stream()
+      .filter(index -> index < x)
+      .mapToInt(Integer::intValue)
+      .count();
+    int expandedX = x + countX * EXPANSION_RATE - countX;
+
+    int countY = (int) verticalIndexes.stream()
+      .filter(index -> index < y)
+      .mapToInt(Integer::intValue)
+      .count();
+    int expandedY = y + countY * EXPANSION_RATE - countY;
+    return new Point(expandedX, expandedY);
   }
 
   public static List<Integer> getAllIndexesOf(String character, String line) {
@@ -65,39 +107,54 @@ public class Day11 {
     return expandUniverseHorizontally(input);
   }
 
-  public static void expandUniverseVertically(List<String> input) {
-    List<String> expansion = new ArrayList<>();
-    for (int i = 0; i < EXPANSION_RATE; i++) {
-      expansion.add(".".repeat(input.get(0).length()));
-    }
-
+  public static List<Integer> getAllVerticalIndexes(List<String> input) {
     List<Integer> indexes = new ArrayList<>();
     for (int i = 0; i < input.size(); i++) {
       if (input.get(i).chars().allMatch(c -> c == '.')) {
-        indexes.add(i + indexes.size() + (EXPANSION_RATE * indexes.size()));
+        indexes.add(i);
       }
     }
-    for (Integer index : indexes) {
-      input.addAll(index, expansion);
-    }
+    return indexes;
   }
 
-  public static List<String> expandUniverseHorizontally(List<String> input) {
-    StringBuilder expansion = new StringBuilder();
-    expansion.append(".".repeat(EXPANSION_RATE));
-
+  public static List<Integer> getAllHorizontalIndexes(List<String> input) {
     List<Integer> indexes = new ArrayList<>();
     for (int i = 0; i < input.get(0).length(); i++) {
       final int temp = i;
       if (input.stream().allMatch(s -> s.charAt(temp) == '.')) {
-        indexes.add(i + indexes.size() + (EXPANSION_RATE * indexes.size()));
+        indexes.add(i);
+      }
+    }
+    return indexes;
+  }
+
+
+
+  public static void expandUniverseVertically(List<String> input) {
+    List<Integer> indexes = new ArrayList<>();
+    for (int i = 0; i < input.size(); i++) {
+      if (input.get(i).chars().allMatch(c -> c == '.')) {
+        indexes.add(i + indexes.size());
+      }
+    }
+    for (Integer index : indexes) {
+      input.add(index, input.get(index));
+    }
+  }
+
+  public static List<String> expandUniverseHorizontally(List<String> input) {
+    List<Integer> indexes = new ArrayList<>();
+    for (int i = 0; i < input.get(0).length(); i++) {
+      final int temp = i;
+      if (input.stream().allMatch(s -> s.charAt(temp) == '.')) {
+        indexes.add(i + indexes.size());
       }
     }
     List<String> newInput = new ArrayList<>();
     for (String line : input) {
       StringBuilder stringBuilder = new StringBuilder(line);
       for (int index : indexes) {
-        stringBuilder.insert(index, expansion);
+        stringBuilder.insert(index, ".");
       }
       newInput.add(stringBuilder.toString());
     }
--- src/test/java/me/zoupis/adventofcode/year2023/Day11Test.java
+++ src/test/java/me/zoupis/adventofcode/year2023/Day11Test.java
@@ -1,4 +1,5 @@
 package me.zoupis.adventofcode.year2023;
 
 public class Day11Test {
+
 }
